29ebe452b6a64318b7b2eef287b39796909abe97
Arefeh Kompani <awriik00@gmail.com>
2021-12-01 15:48:36 +0330

    Add changes of v.2 of document

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const submitName = () => {
+    let name =document.querySelector('input[name="name"]').value
+    let gender =document.querySelector('input[name="gender"]:checked').value
+    console.log(gender);
+}
+
+const submitSave = () => {
+    
+}
+
+const submitClear = () => {
+
+}
