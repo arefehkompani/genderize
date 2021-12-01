7e29338e45a965a8f26c2faf250f2ed620f0b145
Arefeh Kompani <awriik00@gmail.com>
2021-12-01 22:36:41 +0330

    Set limits for form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
-const submitName = () => {
+document.querySelector("#submitName").addEventListener("click",  (event) => {
+    event.preventDefault();
+    let re = /^[a-zA-Z ]+$/
     let name =document.querySelector('input[name="name"]').value
-    let gender =document.querySelector('input[name="gender"]:checked').value
-    console.log(gender);
-}
+    if (name.length>256) {
+        createNotify("You should enter less than 255 character")
+        return 0;
+    }
+    if (!re.test(name)) {
+        createNotify("Please just enter a-z & A-Z")
+    }
+    // let gender =document.querySelector('input[name="gender"]:checked').value
+    // console.log(event);
+})
 
-const submitSave = () => {
-    
+const submitSave = (event) => {
+    event.preventDefault();
+    createNotify("خطا رخ داده است")
 }
 
 const submitClear = () => {
